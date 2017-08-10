6bc99f4b693eaafb85c5820944f26d23eb5d9386
Nicolas Loots <nicolas.loots@outlook.be>
2017-08-10 22:28:25 +0200

    - Gulpfile opgeschoond - Sourcemaps worden niet meer gecreëerd bij de task watch-sass - Ongebruikte packages verwijderd

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,14 @@
 const gulp = require('gulp');
-const rename = require('gulp-rename');
 const gutil = require('gulp-util');
 const uglifyjs = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
+const sass = require('gulp-ruby-sass');
+const filter = require('gulp-filter');
 const browserify = require('browserify');
 const assign = require('lodash.assign');
 const watchify = require('watchify');
 const source = require('vinyl-source-stream');
-const sourcemaps = require('gulp-sourcemaps');
 const buffer = require('vinyl-buffer');
-const sass = require('gulp-ruby-sass');
-const filter = require('gulp-filter');
 
 const DEST = 'build/';
 
@@ -19,7 +17,7 @@ gulp.task('default', ['watch-sass', 'bundle']);
 // Browserify JS + CSS
 
 const params = {
-    entries: ['js/css.js', 'js/functions.js', 'js/map.js','js/tickets.js'],
+    entries: ['js/css.js', 'js/functions.js', 'js/map.js', 'js/tickets.js'],
     debug: true,
     transform: [
         ['browserify-css']
@@ -37,39 +35,20 @@ function bundle() {
         .on('error', gutil.log.bind(gutil, 'Browserify Error'))
         .pipe(source('functions.min.js'))
         .pipe(buffer())
-        // Add transformation tasks to the pipeline here.
         .pipe(uglifyjs())
-        .on('error', function (err) {
+        .on('error', function(err) {
             gutil.log(gutil.colors.red('[Error]'), err.toString());
         })
-        .pipe(sourcemaps.write('./')) // writes .map file
         .pipe(gulp.dest(DEST));
 }
 
-gulp.task('minify-css', () =>
-    gulp.src('css/*.css')
-        .pipe(cleanCSS())
-        .pipe(rename({ extname: '.min.css' }))
-        .pipe(gulp.dest(DEST))
-);
-
 gulp.task('sass', () =>
-    sass('css/styles.scss')
-        .on('error', gutil.log.bind(gutil, 'Sass Error'))
-        .pipe(gulp.dest('css/'))
-        .pipe(filter('**/*.css'))
-
+    sass('css/styles.scss', { sourcemap: false })
+    .on('error', gutil.log.bind(gutil, 'Sass Error'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('css/'))
 );
 
 gulp.task('watch-sass', () =>
     gulp.watch('css/**/*.scss', ['sass'])
 );
-
-gulp.task('minify-watch', () =>
-    gulp.watch('css/**/*.css', ['minify-css'])
-);
-
-gulp.task('node-css', () => {
-    gulp.src('node_modules/font-awesome/*/*')
-        .pipe(gulp.dest(DEST + '/font-awesome'))
-});
