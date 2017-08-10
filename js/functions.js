2a731f1f5e474867306a1573e87d6d8fcf6d6f03
Nicolas Loots <nicolas.loots@outlook.be>
2017-08-10 22:04:20 +0200

    - Side-menu is iets breder - Huidige pagina wordt gehighlight in het menu

--- js/functions.js
+++ js/functions.js
@@ -3,8 +3,7 @@ try {
     window.jQuery = window.$ = require('jquery');
     window.Slideout = require('slideout');
     window.slidesjs = require('../js/jquery.slides');
-} catch (e) {
-}
+} catch (e) {}
 
 var isIE = !!window.MSInputMethodContext && !!document.documentMode;
 var isMobile = screen.width <= 992
@@ -12,12 +11,11 @@ var mainView = {
     storage: null,
     slideout: null,
     location: location.pathname.split('/').slice(-1)[0],
-    init: function () {
+    init: function() {
         this.storage = window.sessionStorage;
         if (history.state != null) {
             this.loadPage(history.state.page);
-        }
-        else this.loadPage('index', true);
+        } else this.loadPage('index', true);
         this.setTitle();
         this.initMenu();
         this.expandInfo();
@@ -31,63 +29,62 @@ var mainView = {
         this.highlightCurrentPage();
         this.windowResize();
     },
-    setTitle: function () {
+    setTitle: function() {
         $("#title").text(document.title.substring(document.title.lastIndexOf(" ")));
     },
 
-    windowResize: function () {
-        $(window).resize(function () {
+    windowResize: function() {
+        $(window).resize(function() {
             if (window.innerWidth > 992) {
                 $("#menu").show();
-                this.slideout.disableTouch();
-            }
-            else {
+                mainView.slideout.disableTouch();
+            } else {
                 $("header").css("opacity", 1);
                 mainView.slideout();
                 $("#menu").hide();
-                this.slideout.enableTouch();
+                mainView.slideout.enableTouch();
             }
         });
     },
-    initMenu: function () {
+    initMenu: function() {
         this.slideout = new Slideout({
             'panel': document.getElementById('panel'),
             'menu': document.getElementById('menu'),
-            'padding': 250,
+            'padding': 240,
             'tolerance': 70
         });
         if (window.innerWidth > 992) {
             mainView.slideout.disableTouch();
         }
         // Toggle button
-        document.querySelector('.toggle-button').addEventListener('click', function () {
+        document.querySelector('.toggle-button').addEventListener('click', function() {
             mainView.slideout.toggle();
         });
 
-        $('#menu').find('a').each(function () {
-            $(this).click(function (e) {
+        $('#menu').find('a').each(function() {
+            $(this).click(function(e) {
                 e.preventDefault();
-                //mainView.changeSelected($(this));
+                mainView.changeSelected($(this));
                 if ($(this).attr('href')) {
                     mainView.loadPage($(this).attr('href').split('#')[1], true);
                 }
             });
         });
     },
-    loadPage: function (page, push) {
-        $('#main').load('html/' + page + '.php', function () {
-            switch(page){
+    loadPage: function(page, push) {
+        $('#main').load('html/' + page + '.php', function() {
+            switch (page) {
                 case "index":
                     mainView.countdownInit(new Date('2017-03-18T21:00:00'));
                     setInterval(this.countdownInit, 1000);
                     break;
                 case "tickets":
-                    $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDxFQATxIl21PpSjcu_dzg-PT7GzQwsyEc", function () {
+                    $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDxFQATxIl21PpSjcu_dzg-PT7GzQwsyEc", function() {
                         initTickets();
                     });
                     break;
                 case "location":
-                    $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDxFQATxIl21PpSjcu_dzg-PT7GzQwsyEc", function () {
+                    $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDxFQATxIl21PpSjcu_dzg-PT7GzQwsyEc", function() {
                         initMap();
                     });
                     break;
@@ -106,44 +103,49 @@ var mainView = {
                     page: page
                 }, null, page);
             }
+            mainView.changeSelected($('#menu').find('a[href="#' + page + '"]'));
             //mainView.setHeaderText();
             mainView.setTitle();
         });
     },
-    setStorage: function () {
+    changeSelected: function(elem) {
+        $('.active').removeClass('active');
+        $(elem).addClass('active');
+    },
+    setStorage: function() {
         mainView.storage.setItem("anim", "true");
     },
-    checkStorage: function () {
+    checkStorage: function() {
         return mainView.storage.length > 0;
     },
-    animateLogo: function () {
-        $("#anim").toggle().delay(1000).animate({top: "-100%"}, 1000);
+    animateLogo: function() {
+        $("#anim").toggle().delay(1000).animate({ top: "-100%" }, 1000);
     },
 
-    displayContent: function () {
+    displayContent: function() {
         var panelTO = (window.innerWidth > 992) ? 3000 : 2000;
         $("#panel").delay(panelTO).fadeTo(1000, 1);
         if (window.innerWidth > 992) {
             $(".slideout-menu").delay(2000).fadeTo(1000, 1);
         }
     },
-    resetAnim: function () {
+    resetAnim: function() {
         if (window.innerWidth > 992) {
             $("#menu").show();
         }
         $("#panel").css("opacity", 1);
     },
-    expandInfo: function () {
-        $(".dj").click(function () {
+    expandInfo: function() {
+        $(".dj").click(function() {
             $(".exp").not($(this).next()).delay(500).slideUp(500);
             $(this).next().slideToggle(500);
         });
     },
-    highlightCurrentPage: function () {
+    highlightCurrentPage: function() {
         if (this.location === "") {
             $("#menu").find("a[href]").first().parent("li").addClass("active")
         } else {
-            $("#menu").find("a[href]").each(function () {
+            $("#menu").find("a[href]").each(function() {
                 if (this.href === window.location.href) {
                     $(this).parent("li").addClass("active");
                 }
@@ -151,11 +153,11 @@ var mainView = {
         }
     },
     setMapDimensions: function() {
-        if (window.innerWidth < 992){
+        if (window.innerWidth < 992) {
             $("#map").width(window.innerWidth - 40);
         }
     },
-    countdownInit: function (date) {
+    countdownInit: function(date) {
         var t = date - new Date();
         if (t > 0) {
             var sec = Math.floor((t / 1000) % 60);
@@ -170,15 +172,15 @@ var mainView = {
     }
 };
 
-window.onload = function () {
+window.onload = function() {
     mainView.init();
 };
 
-window.onpopstate = function (event) {
+window.onpopstate = function(event) {
     var page = 'index';
     if (event.state) {
         page = event.state.page;
     }
     mainView.loadPage(page);
-    //mainView.changeSelected($('#menu').find('a[href="#' + page + '"]'));
+
 };
