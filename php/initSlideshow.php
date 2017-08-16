e4e56b3562fe03580d4197cf5be32e8a923ac108
Nicolas Loots <nicolas.loots@outlook.be>
2017-08-16 11:55:57 +0200

    Slideshow-images krijgen nu het juiste src-attribuut mee

--- php/initSlideshow.php
+++ php/initSlideshow.php
@@ -3,6 +3,7 @@ $imgs = glob("../images/sponsors/*.*");
 shuffle($imgs);
 $res='';
 foreach ($imgs as $file) {
-    $res .= '<img src="' . $file . '" alt="' . substr($file, strrpos($file, '/') + 1) . '">';
+    $fixpath = str_replace('../','',$file);
+    $res .= '<img src="' . $fixpath . '" alt="' . substr($file, strrpos($file, '/') + 1) . '">';
 }
 echo $res;
