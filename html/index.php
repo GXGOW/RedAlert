27f1df3c7bd1486fd27848701ac3494ae6fd49b8
Nicolas Loots <nicolas.loots@outlook.be>
2017-10-17 20:45:54 +0200

    Editie 2018!

--- html/index.php
+++ html/index.php
@@ -1,15 +1,10 @@
 <picture>
-    <img src="images/thanks.jpg" alt="Sfeerfoto">
+    <img src="images/teaser.jpg" alt="Sfeerfoto">
 </picture>
 <div id="border">
-    <h2>6<sup>de</sup> editie Red Alert!</h2>
-    <p>Het was weer een dik geslaagde editie! Bedankt aan iedereen die gekomen is en geholpen heeft om er weer
-        een dik feestje van te maken! De foto's zijn ondertussen
-        <a href="https://www.facebook.com/pg/LieseKeeremanPhotography/photos/?tab=album&album_id=639048146290817" target="_blank">hier</a> te vinden.</p>
-    <p>Bedankt en tot volgend jaar!</p>
-
-    <!--<p><a href="http://kljhamme.be/" target="_blank">KLJ Hamme-Center</a> organiseert voor de zesde keer een
-        van de grootste knallers van het Hamse fuifseizoen! Op zaterdag 18 maart 2017 kan je in zaal
+    <h2>7<sup>de</sup> editie Red Alert!</h2>
+    <p><a href="http://kljhamme.be/" target="_blank">KLJ Hamme-Center</a> organiseert voor de zevende keer een
+        van de grootste knallers van het Hamse fuifseizoen! Op zaterdag 3 maart 2018 kan je in zaal
         Hambiance weer komen feesten met Red Alert! Aankondiging van de DJ's komt nog, dus jullie zullen nog
         even in spanning moeten afwachten...
     </p>
@@ -17,5 +12,5 @@
         namelijk twee consumpties voor de prijs van een!</p>
     <p>Klik op een pagina in het menu links voor tickets en info. Hou ook zeker onze <a href="https://www.facebook.com/RedAlertHamme/?fref=ts">Facebookpagina</a> in de
         gaten en <strong>maak kans op gratis tickets!</strong></p>
-    <p>Het was weer een dik geslaagde editie dit jaar! Bedankt aan iedereen die gekomen is. De foto's zijn <a href=""</p>-->
+        <p>De foto's van onze vorige editie zijn <a href="https://www.facebook.com/pg/LieseKeeremanPhotography/photos/?tab=album&album_id=639048146290817" target="_blank">hier</a> te bekijken!</p>
 </div>
