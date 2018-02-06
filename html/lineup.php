c4415450a19703983f70eba8d9104a2a62a31417
Nicolas Loots <nicolas@Nico-ARCH.localdomain>
2018-02-06 14:00:22 +0100

    Lineup bijgewerkt

--- html/lineup.php
+++ html/lineup.php
@@ -1,13 +1,13 @@
 <div id="border">
     <div id="djwrap">
         <div id='headliners'>
-            <img src="images/lineup/template.png" />
-            <img src="images/lineup/template.png" />
+            <img id="hl1" src="images/lineup/lereujs.jpg" />
+            <img id="hl2" src="images/lineup/lesterwilliams.jpg" />
         </div>
         <div id='djs'>
             <img id="dj1" src="images/lineup/mrmore.jpg" />
-            <img src="images/lineup/template.png" />
-            <img src="images/lineup/template.png" />
+            <img id="dj2" src="images/lineup/notrixx.jpg" />
+            <img id="dj3" src="images/lineup/fvb.jpg" />
         </div>
         <div id="djinfo">
         </div>
